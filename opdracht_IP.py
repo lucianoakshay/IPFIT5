5949214a8e1c791610f0e3e331e7a41b2d72e2e5
koen404 <koenvandersijs@hotmail.com>
2018-02-13 14:13:40 +0100

    opbouw IP opdracht

--- opdracht_IP.py
+++ opdracht_IP.py
@@ -1,6 +1,8 @@
 import dpkt
-import hashlib
-
+import hashlib as hash
+import os
+import sys
+BUFFERSIZE =65536
 def open_bestand(bestanden, aantal):
     lijst = []
     #fout afhandeling
@@ -9,14 +11,21 @@ def open_bestand(bestanden, aantal):
 
 def bereken_hash(bestand):
 
+    sha256hash = hash.sha3_256()
+    with open(os.path.join(sys.path[0]+ '\\'+ bestand),'rb') as file:
+        file_buffer = file.read(BUFFERSIZE)
+        while len(file_buffer)>0:
+            sha256hash.update(file_buffer)
+            file_buffer= file.read(BUFFERSIZE)
+    print(sha256hash.hexdigest())
 
 
 
 
+    # bestand =bestand
+    # file = open(bestand, 'r')
+    # pcap =dpkt.pcap.Reader(file)
+    # for ts, buf in pcap:
+    #     eth = dpkt.ethernet.Ethernet(buf)
 
-    bestand =bestand
-    file = open(bestand, 'r')
-    pcap =dpkt.pcap.Reader(file)
-    for ts, buf in pcap:
-        eth = dpkt.ethernet.Ethernet(buf)
-
+bereken_hash('test.py')
