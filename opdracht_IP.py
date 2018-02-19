ea9b576d960e6db52bdf2e3dcaa2ecbcfa84e1b9
koen404 <koenvandersijs@hotmail.com>
2018-02-19 09:39:18 +0100

    IP analyse
    To do:
    
    error handling
    private range filtering
    logging

--- opdracht_IP.py
+++ opdracht_IP.py
@@ -20,204 +20,205 @@ from tld import get_tld
 
 import socket
 from collections import Counter
+class IP_filtering:
+    def __init__(self):
+        print()
+    def open_bestand(self,aantal):
+        lijst = []
+        #fout afhandeling
+        for i in range(aantal):
+            lijst.append(input('geef bestand op'))
+            self.Filter_IP(lijst)
 
-def open_bestand(aantal):
-    lijst = []
-    #fout afhandeling
-    for i in range(aantal):
-        lijst.append(input('geef bestand op'))
-        Filter_IP(lijst)
-
-# moet in main zodat je het hier kan aanroepen voor elk input bestand.
-
-
-
-def Filter_IP (bestand):
-    IP_list=Counter()
-    temp_ip=''
-    bestand=bestand
-    # for bestand in bestanden:
-
-    pcap =open (os.path.join(sys.path[0], bestand),'rb')
-    pcap = dpkt.pcap.Reader(pcap)
-    for timestamp,buf in pcap:
-        eth = dpkt.ethernet.Ethernet(buf)
-        if eth.type == dpkt.ethernet.ETH_TYPE_IP6:
-            ipv6=eth.data
-            IP_list[convert_IP(ipv6.src)]+=1
-            IP_list[convert_IP(ipv6.dst)]+=1
-            print(convert_IP(ipv6.src))
-            print(convert_IP(ipv6.dst))
-            continue
-
-        ip=eth.data
-
-        if not isinstance(eth.data, dpkt.ip.IP):
-           print('Non IP Packet type not supported %s\n' % eth.data.__class__.__name__)
-           continue
-        IP_list[convert_IP(ip.src)]+=1
-        IP_list[convert_IP(ip.dst)]+=1
-    return IP_list
-
-def convert_IP(ip_adress):
-    try:
-        return socket.inet_ntop(socket.AF_INET, ip_adress)
-    except ValueError:
-        return socket.inet_ntop(socket.AF_INET6, ip_adress)
-
-def write (filename,input):
-    aantal = ''
-    file = open(os.path.join(sys.path[0],strftime("%Y-%m-%d %H:%M", gmtime()))+'IP', 'w+')
-    file.write(filename+ ':')
-    file.write('IP-address      count' +'\n')
-    for ip,waarde in input.items():
-        print(ip)
-        file.write( '{0:<16} {1:>8}'.format(ip,str(waarde))+'\n')
-    file.close()
-
-def compare(input_lijst,IPlijst):
-    temp_lijst = []
-    match_list=[]
-    print(list(IPlijst.keys())[0])
-    bestand = open(os.path.join(sys.path[0],input_lijst),'r')
-    for rij in bestand:
-        rij = rij.strip('\n')
-        temp_lijst.append(rij)
-    if temp_lijst:
-        for ip in list(IPlijst.keys()):
-            if ip in  temp_lijst:
-                match_list.append(ip)
-    else:
-        print("file is empty")
-    if match_list:
-        return(match_list)
-    else:
-        print("no match")
-    bestand.close()
+    # moet in main zodat je het hier kan aanroepen voor elk input bestand.
 
 
-def json_time_converter(timestamp):
-    if isinstance(timestamp, datetime.datetime):
-        return timestamp.__str__()
 
-def WHOIS(match_list):
-    print (match_list)
+    def Filter_IP (self,bestand):
+        IP_list=Counter()
+        temp_ip=''
+        bestand=bestand
+        # for bestand in bestanden:
 
-    for ip in match_list:
-        indicator = {}
-
-        with open(os.path.join(sys.path[0],'output2.json'), 'at') as file:
-            obj = IPWhois(ip)
-            results = obj.lookup_rdap(depth=1)
-            print(results)
-            indicator['Queried_IP:']=ip
-            indicator['Queried_domain']=socket.getfqdn(ip)
-            #nog opzoek naar een betere whois, kan raw evt weglaten.
-            dict =(pythonwhois.get_whois(get_tld('http://'+socket.getfqdn(ip))))
-
-            print(DIG(get_tld('http://'+socket.getfqdn(ip))))
-            # print (dict)
-            indicator.update(dict)
-            # string = string.strip('['+']'+"\\n")
-            file.write(json.dumps(indicator,default=json_time_converter))
-
-def DIG(domain):
-    ids = [
-        'NONE',
-        'A',
-        'NS',
-        'MD',
-        'MF',
-        'CNAME',
-        'SOA',
-        'MB',
-        'MG',
-        'MR',
-        'NULL',
-        'WKS',
-        'PTR',
-        'HINFO',
-        'MINFO',
-        'MX',
-        'TXT',
-        'RP',
-        'AFSDB',
-        'X25',
-        'ISDN',
-        'RT',
-        'NSAP',
-        'NSAP-PTR',
-        'SIG',
-        'KEY',
-        'PX',
-        'GPOS',
-        'AAAA',
-        'LOC',
-        'NXT',
-        'SRV',
-        'NAPTR',
-        'KX',
-        'CERT',
-        'A6',
-        'DNAME',
-        'OPT',
-        'APL',
-        'DS',
-        'SSHFP',
-        'IPSECKEY',
-        'RRSIG',
-        'NSEC',
-        'DNSKEY',
-        'DHCID',
-        'NSEC3',
-        'NSEC3PARAM',
-        'TLSA',
-        'HIP',
-        'CDS',
-        'CDNSKEY',
-        'CSYNC',
-        'SPF',
-        'UNSPEC',
-        'EUI48',
-        'EUI64',
-        'TKEY',
-        'TSIG',
-        'IXFR',
-        'AXFR',
-        'MAILB',
-        'MAILA',
-        'ANY',
-        'URI',
-        'CAA',
-        'TA',
-        'DLV',
-    ]
-
-    for record in ids:
-        try:
-            answers = dns.resolver.query(domain, record)
-            for rdata in answers:
-                print(record, ':', rdata.to_text())
-
-        except Exception as e:
-            continue  # or pass
-def timeline(bestand,ip_list):
-    pcap =open (os.path.join(sys.path[0], bestand),'rb')
-    pcap = dpkt.pcap.Reader(pcap)
-
-    for timestamp,buf in pcap:
-        eth = dpkt.ethernet.Ethernet(buf)
-        for ip in ip_list:
+        pcap =open (os.path.join(sys.path[0], bestand),'rb')
+        pcap = dpkt.pcap.Reader(pcap)
+        for timestamp,buf in pcap:
+            eth = dpkt.ethernet.Ethernet(buf)
             if eth.type == dpkt.ethernet.ETH_TYPE_IP6:
                 ipv6=eth.data
-                if convert_IP(ipv6.src)==ip:
-                    print(ipv6)
-            if eth.type ==dpkt.ethernet.ETH_TYPE_IP:
-                ipv4 = eth.data
-                if(convert_IP(ipv4.src))==ip:
-                    print( 'Timestamp: ', str(datetime.datetime.utcfromtimestamp(timestamp)))
-                    print( 'IP: %s -> %s   (len=%d ttl=%d)\n' % \
-          (convert_IP(ipv4.src), convert_IP(ipv4.dst), ipv4.len, ipv4.ttl))
+                IP_list[self.convert_IP(ipv6.src)]+=1
+                IP_list[self.convert_IP(ipv6.dst)]+=1
+                print(self.convert_IP(ipv6.src))
+                print(self.convert_IP(ipv6.dst))
+                continue
+
+            ip=eth.data
+
+            if not isinstance(eth.data, dpkt.ip.IP):
+               print('Non IP Packet type not supported %s\n' % eth.data.__class__.__name__)
+               continue
+            IP_list[self.convert_IP(ip.src)]+=1
+            IP_list[self.convert_IP(ip.dst)]+=1
+        return IP_list
+
+    def convert_IP(ip_adress):
+        try:
+            return socket.inet_ntop(socket.AF_INET, ip_adress)
+        except ValueError:
+            return socket.inet_ntop(socket.AF_INET6, ip_adress)
+
+    def write (filename,input):
+        file = open(os.path.join(sys.path[0],strftime("%Y-%m-%d %H:%M", gmtime()))+'IP', 'w+')
+        file.write(filename+ ':')
+        file.write('IP-address      count' +'\n')
+        for ip,waarde in input.items():
+            print(ip)
+            file.write( '{0:<16} {1:>8}'.format(ip,str(waarde))+'\n')
+        file.close()
+
+    def compare(self,input_lijst,IPlijst):
+        temp_lijst = []
+        match_list=[]
+        print(list(IPlijst.keys())[0])
+        bestand = open(os.path.join(sys.path[0],input_lijst),'r')
+        for rij in bestand:
+            rij = rij.strip('\n')
+            temp_lijst.append(rij)
+        if temp_lijst:
+            for ip in list(IPlijst.keys()):
+                if ip in  temp_lijst:
+                    match_list.append(ip)
+        else:
+            print("file is empty")
+        if match_list:
+            return(match_list)
+        else:
+            print("no match")
+        bestand.close()
+
+
+    def json_time_converter(self,timestamp):
+        if isinstance(timestamp, datetime.datetime):
+            return timestamp.__str__()
+
+    def WHOIS(self,match_list):
+        print (match_list)
+
+        for ip in match_list:
+            indicator = {}
+
+            with open(os.path.join(sys.path[0],'output2.json'), 'at') as file:
+                obj = IPWhois(ip)
+                results = obj.lookup_rdap(depth=1)
+                print(results)
+                indicator['Queried_IP:']=ip
+                indicator['Queried_domain']=socket.getfqdn(ip)
+                #nog opzoek naar een betere whois, kan raw evt weglaten.
+                dict =(pythonwhois.get_whois(get_tld('http://'+socket.getfqdn(ip))))
+
+                print(self.DIG(get_tld('http://'+socket.getfqdn(ip))))
+                # print (dict)
+                indicator.update(dict)
+                # string = string.strip('['+']'+"\\n")
+                file.write(json.dumps(indicator,default=self.json_time_converter))
+
+    def DIG(self,domain):
+        ids = [
+            'NONE',
+            'A',
+            'NS',
+            'MD',
+            'MF',
+            'CNAME',
+            'SOA',
+            'MB',
+            'MG',
+            'MR',
+            'NULL',
+            'WKS',
+            'PTR',
+            'HINFO',
+            'MINFO',
+            'MX',
+            'TXT',
+            'RP',
+            'AFSDB',
+            'X25',
+            'ISDN',
+            'RT',
+            'NSAP',
+            'NSAP-PTR',
+            'SIG',
+            'KEY',
+            'PX',
+            'GPOS',
+            'AAAA',
+            'LOC',
+            'NXT',
+            'SRV',
+            'NAPTR',
+            'KX',
+            'CERT',
+            'A6',
+            'DNAME',
+            'OPT',
+            'APL',
+            'DS',
+            'SSHFP',
+            'IPSECKEY',
+            'RRSIG',
+            'NSEC',
+            'DNSKEY',
+            'DHCID',
+            'NSEC3',
+            'NSEC3PARAM',
+            'TLSA',
+            'HIP',
+            'CDS',
+            'CDNSKEY',
+            'CSYNC',
+            'SPF',
+            'UNSPEC',
+            'EUI48',
+            'EUI64',
+            'TKEY',
+            'TSIG',
+            'IXFR',
+            'AXFR',
+            'MAILB',
+            'MAILA',
+            'ANY',
+            'URI',
+            'CAA',
+            'TA',
+            'DLV',
+        ]
+
+        for record in ids:
+            try:
+                answers = dns.resolver.query(domain, record)
+                for rdata in answers:
+                    print(record, ':', rdata.to_text())
+
+            except Exception as e:
+                continue  # or pass
+    def timeline(self,bestand,ip_list):
+        pcap =open (os.path.join(sys.path[0], bestand),'rb')
+        pcap = dpkt.pcap.Reader(pcap)
+
+        for timestamp,buf in pcap:
+            eth = dpkt.ethernet.Ethernet(buf)
+            for ip in ip_list:
+                if eth.type == dpkt.ethernet.ETH_TYPE_IP6:
+                    ipv6=eth.data
+                    if self.convert_IP(ipv6.src)==ip:
+                        print(ipv6)
+                if eth.type ==dpkt.ethernet.ETH_TYPE_IP:
+                    ipv4 = eth.data
+                    if(self.convert_IP(ipv4.src))==ip:
+                        print( 'Timestamp: ', str(datetime.datetime.utcfromtimestamp(timestamp)))
+                        print( 'IP: %s -> %s   (len=%d ttl=%d)\n' % \
+              (self.convert_IP(ipv4.src), self.convert_IP(ipv4.dst), ipv4.len, ipv4.ttl))
 
 timeline('003hslmwa.pcap',compare('IP.txt',Filter_IP('003hslmwa.pcap')))
     # 184.50.160.199
