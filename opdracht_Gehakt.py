5ac7c5ced9dff9444bdd842fe075e71a94691657
devsda12 <jmbalster@hotmail.com>
2018-03-06 13:56:48 +0100

    Made first code in Gehakt. Now makes a dictionary with names coupled to extensions. Dictionary with sha256 hashes coupled with files to come.

---
+++ opdracht_Gehakt.py
@@ -0,0 +1,21 @@
+import os
+
+#User input requested
+user_input = input("Voer de master directory in: ")
+
+def file_list(input):
+    #Dictionary om alle files met hashes erbij op te slaan
+    file_extension_dict = {}
+    file_hash_dict = {}
+
+    # Loop die een variabele met het path naar een file update zodat iedere file in de directory wordt afgelopen.
+    # Vervolgens worden de subdir en de file gejoind in de variabele current_dir
+    # Hierna wordt een betreffende directory meegegeven aan een splitext commando die de extensie van de file afhaald
+    for subdir, dirs, files in os.walk(user_input):
+        for file in files:
+            current_dir = (os.path.join(subdir, file))
+            filename, file_extension = os.path.splitext(current_dir)
+            file_extension_dict[filename] = file_extension
+            print(file_extension_dict)
+
+file_list(user_input)
