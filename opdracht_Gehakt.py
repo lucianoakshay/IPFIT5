735c5fb0797d695c6dad9cbfee2965c38ba38693
devsda12 <jmbalster@hotmail.com>
2018-03-06 14:09:38 +0100

    Adjusted dictionary's so the hash and extension are in one dictionary per file.

--- opdracht_Gehakt.py
+++ opdracht_Gehakt.py
@@ -5,9 +5,8 @@ user_input = input("Voer de master directory in: ")
 
 def file_list(input):
     #Dictionary om alle files met hashes erbij op te slaan
-    file_extension_dict = {}
-    file_hash_dict = {}
-
+    file_dict = {}
+    hashie = 0
     # Loop die een variabele met het path naar een file update zodat iedere file in de directory wordt afgelopen.
     # Vervolgens worden de subdir en de file gejoind in de variabele current_dir
     # Hierna wordt een betreffende directory meegegeven aan een splitext commando die de extensie van de file afhaald
@@ -15,7 +14,7 @@ def file_list(input):
         for file in files:
             current_dir = (os.path.join(subdir, file))
             filename, file_extension = os.path.splitext(current_dir)
-            file_extension_dict[filename] = file_extension
-            print(file_extension_dict)
+            file_dict[filename] = {"Extension":file_extension, "Hash value":hashie}
+            print(file_dict)
 
 file_list(user_input)
