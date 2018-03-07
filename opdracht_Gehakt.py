f8d72f9ca60da5c08013d69082f9976ed0b55e02
devsda12 <jmbalster@hotmail.com>
2018-03-07 14:40:34 +0100

    Gehakt variables aangepast

--- opdracht_Gehakt.py
+++ opdracht_Gehakt.py
@@ -7,16 +7,15 @@ def file_list(input):
     test = User_Interface.Main_program()
     #Dictionary om alle files met hashes erbij op te slaan
     file_dict = {}
-    hashie = 0
     # Loop die een variabele met het path naar een file update zodat iedere file in de directory wordt afgelopen.
     # Vervolgens worden de subdir en de file gejoind in de variabele current_dir
     # Hierna wordt een betreffende directory meegegeven aan een splitext commando die de extensie van de file afhaald
     for subdir, dirs, files in os.walk(user_input):
         for file in files:
             current_dir = (os.path.join(subdir, file))
-            hashie = User_Interface.Main_program.bereken_hash(test,current_dir)
+            hash_waarde = User_Interface.Main_program.bereken_hash(test,current_dir)
             filename, file_extension = os.path.splitext(current_dir)
-            file_dict[filename] = {"Extension":file_extension, "Hash value":hashie}
+            file_dict[filename] = {"Extension":file_extension, "Hash value":hash_waarde}
         print(file_dict)
 
 
