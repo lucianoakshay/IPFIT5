49d55f268e0ea31c032770542838862ae4c977c4
koen <koenvandersijs@hotmail.com>
2018-03-10 21:34:19 +0100

    Made changes to timeline, can now print port number.

--- opdracht_IP.py
+++ opdracht_IP.py
@@ -102,44 +102,32 @@ class IP_filtering:
             'DLV',
         ]
 
+
     def __init__(self):
+        self.compare_input= None
+        self.bestanden = None
+        print()
         # self.choices = {
         #         "1": self.file_input,
         #         "2": self.back,
         #         "3": self.quit
         #         }
-        print()
-    # deprecated function will possibly be removed
-    def display_menu(self):
-                print("""
-Menu
-1. Input_file
-2. Go_back
-3. Quit
-""")
 
-    # def run_ip(self):
-    #
-    #     while True:
-    #         self.display_menu()
-    #         User_Interface.Main_program().Logging().info("testing")
-    #         choice = input("Enter an option: ")
-    #         action = self.choices.get(choice)
-    #         if action:
-    #             action()
-    #         else:
-    #             print("{0} is not a valid choice".format(choice))
-    #
-    # def back(self):
+
+
     #     User_Interface.Main_program().run()
-    #
-    # def quit(self):
-    #     sys.exit(0)
+
+    def quit(self):
+        sys.exit(0)
+
     # also absolete class this will be handeled in the user interface class
+
+    def set_compare(self,input_compare):
+        self.compare_input = input_compare
+
     def file_input(self):
         lijst = []
         dictionary={}
-        dictionary2={}
         aantal = int(input('Input the ammount of files'))
         #fout afhandeling
         for i in range(aantal):
@@ -149,7 +137,8 @@ Menu
         #onderstaande kan in Filter_IP script
         dictionary.update(self.Filter_IP(lijst))
         print(dictionary)
-        self.write('test', dictionary)
+        self.write( dictionary)
+
         # determine if it's a relative path or an absolute path
     # moet in main zodat je het hier kan aanroepen voor elk input bestand.
 
@@ -157,11 +146,14 @@ Menu
     # class that will filter the IP adresses that resides in an pcap file
     def Filter_IP (self, bestanden):
         IP_list=Counter()
+        temp_list =[]
+        match_list ={}
         hashes={}
         temp_ip=''
-        bestanden=bestanden
-        print(bestanden)
-        for bestand in bestanden:
+        self.bestanden=bestanden
+        print(self.bestanden)
+
+        for bestand in self.bestanden:
             # hashes[bestand]=User_Interface.Main_program.bereken_hash(bestand)
 
             # for bestand in bestanden:
@@ -170,14 +162,18 @@ Menu
             pcap =open (os.path.join(sys.path[0], bestand),'rb')
             pcap = dpkt.pcap.Reader(pcap)
             for timestamp,buf in pcap:
+
+
                 eth = dpkt.ethernet.Ethernet(buf)
                 if eth.type == dpkt.ethernet.ETH_TYPE_IP6:
                     ipv6=eth.data
                     IP_list[self.convert_IP(ipv6.src)]+=1
                     IP_list[self.convert_IP(ipv6.dst)]+=1
+
+
                     # print(self.convert_IP(ipv6.src))
                     # print(self.convert_IP(ipv6.dst))
-                    continue
+
 
                 ip=eth.data
 
@@ -186,7 +182,8 @@ Menu
                    continue
                 IP_list[self.convert_IP(ip.src)]+=1
                 IP_list[self.convert_IP(ip.dst)]+=1
-        print( IP_list)
+        # print(IP_list)
+        return IP_list
     # will be used to convert an hex ip to an human readable format
     def convert_IP(self,ip_adress):
         try:
@@ -195,41 +192,51 @@ Menu
             return socket.inet_ntop(socket.AF_INET6, ip_adress)
 
     #  will be used to write out the list of IP-adreses
-    def write (self, filename, input):
+    def write (self,input):
         with open(os.path.join(sys.path[0],str(date.today())+'.txt'),'w+') as file:
             # file.write(filename + ':')strftime("%Y-%m-%d %H:%M", gmtime()))
             file.write('IP-address      count' +'\n')
             for ip, waarde in input.items():
                 print(ip)
-                print('test')
                 print(waarde)
                 file.write( '{0:<16} {1:>8}'.format(ip,str(waarde))+'\n')
             file.flush()
+        file.close()
     # function that will compare the list of IP-adresses to the IP-adresses inside the pcap file
-    def compare(self, input_lijst, IPlijst):
+    def compare(self, IPlijst):
+        print(self.compare_input)
         temp_lijst = []
         match_list=[]
-        print(list(IPlijst.keys())[0])
-        bestand = open(os.path.join(sys.path[0],input_lijst),'r')
+        # print(list(IPlijst.keys())[0])
+
+        bestand = open(os.path.join(sys.path[0],self.compare_input),'r')
         for rij in bestand:
+            print(rij)
             rij = rij.strip('\n')
             temp_lijst.append(rij)
         if temp_lijst:
             for ip in list(IPlijst.keys()):
+                # print(ip)
                 if ip in temp_lijst:
+                    # print(ip)
                     match_list.append(ip)
         else:
             print("file is empty")
         if match_list:
+            print(match_list)
+            print(self.bestanden)
             return match_list
         else:
             print("no match")
         bestand.close()
+        # else:
+        #     User_Interface.Main_program().run()
 
 
     def json_time_converter(self, timestamp):
         if isinstance(timestamp, datetime.datetime):
             return timestamp.__str__()
+
     # function to get the whois information about an
     def WHOIS(self,match_list):
         print (match_list)
@@ -266,36 +273,50 @@ Menu
     # dit is dubbel op kan waarschijnlijk verplaatst worden naar de functie die het pcap bestand opent.
 
     # function to create a timelin, needs to be adjusted so that it can show the ports and the protocol.
-    def timeline(self, bestand, ip_list):
-        pcap =open (os.path.join(sys.path[0], bestand),'rb')
-        pcap = dpkt.pcap.Reader(pcap)
+    # note to self ip_list is an list object and is not a file location. Maybe it would be useful to read the
+    def timeline(self, ip_list):
+        print(self.bestanden)
+        for bestand in self.bestanden:
+            print(bestand)
+            pcap =open (os.path.join(sys.path[0], bestand),'rb')
+            pcap = dpkt.pcap.Reader(pcap)
 
-        for timestamp,buf in pcap:
-            eth = dpkt.ethernet.Ethernet(buf)
-            for ip in ip_list:
-                if eth.type == dpkt.ethernet.ETH_TYPE_IP6:
-                    ipv6=eth.data
-                    if self.convert_IP(ipv6.src) == ip:
-                        print( 'Timestamp: ', str(datetime.datetime.utcfromtimestamp(timestamp)))
-                        print( 'IP: %s -> %s   (len=%d ttl=%d)\n' % \
-              (self.convert_IP(ipv6.src), self.convert_IP(ipv6.dst), ipv6.len, ipv6.ttl))
+            for timestamp,buf in pcap:
+                eth = dpkt.ethernet.Ethernet(buf)
+                for ip in ip_list:
+                    # tcp = ip.data
+                    # print (ip)
+                    if eth.type == dpkt.ethernet.ETH_TYPE_IP6:
+                        ipv6=eth.data
+                        if self.convert_IP(ipv6.src) == ip:
+                            print( 'Timestamp: ', str(datetime.datetime.utcfromtimestamp(timestamp)))
+                            print( 'IP: %s -> %s   (len=%d ttl=%d)\n' % \
+                  (self.convert_IP(ipv6.src), self.convert_IP(ipv6.dst), ipv6.len, ipv6.ttl))
 
-                if eth.type ==dpkt.ethernet.ETH_TYPE_IP:
-                    ipv4 = eth.data
+                    if eth.type ==dpkt.ethernet.ETH_TYPE_IP:
+                        ipv4 = eth.data
+                        # also check if IP.dst is equal to ip
+                        if(self.convert_IP(ipv4.src)) == ip:
+                            print( 'Timestamp: ', str(datetime.datetime.utcfromtimestamp(timestamp)))
+                            print( 'IP: %s -> %s   (len=%d ttl=%d)\n' % \
+                            (self.convert_IP(ipv4.src), self.convert_IP(ipv4.dst), ipv4.len, ipv4.ttl))
+                            if not isinstance(ipv4.data,dpkt.icmp.ICMP):
+                                udp =ipv4.data
+                                port = udp.sport
+                                print("port:"+ str(port))
 
-                    if(self.convert_IP(ipv4.src)) == ip:
-                        print( 'Timestamp: ', str(datetime.datetime.utcfromtimestamp(timestamp)))
-                        print( 'IP: %s -> %s   (len=%d ttl=%d)\n' % \
-                        (self.convert_IP(ipv4.src), self.convert_IP(ipv4.dst), ipv4.len, ipv4.ttl))
 
+# test = IP_filtering()
+# IP_filtering.timeline(test,"C:\\Users\\kvandersijs\\PycharmProjects\\IPFIT5\\003hslmwa.pcap","C:\\Users\\kvandersijs\\PycharmProjects\\IPFIT5\\IP.txt")
 
 
 # if __name__ == "__main__":
 #     IP_filtering().run_ip()
-# timeline('003hslmwa.pcap',compare('IP.txt',Filter_IP('003hslmwa.pcap')))
-    # 184.50.160.199
-    # bestand =bestand
-    # file = open(bestand, 'r')
-    # pcap =dpkt.pcap.Reader(file)
-    # for ts, buf in pcap:
-    #eth = dpkt.ethernet.Ethernet(buf)
+#     # self.timeline('003hslmwa.pcap',compare('IP.txt',Filter_IP('003hslmwa.pcap')))
+#
+#     # 184.50.160.199
+#     # bestand =bestand
+#     # file = open(bestand, 'r')
+#     # pcap =dpkt.pcap.Reader(file)
+#     # for ts, buf in pcap:
+#     #eth = dpkt.ethernet.Ethernet(buf)
