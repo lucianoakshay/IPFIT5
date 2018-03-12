df04c2b93a136e9f7ee4f8ca20ccb4a15cc9d7e3
jasper <jmbalster@hotmail.com>
2018-03-12 17:48:24 +0100

    Made new function to load the dd image

--- opdracht_Gehakt.py
+++ opdracht_Gehakt.py
@@ -1,16 +1,34 @@
 import os
 import User_Interface
 from tqdm import tqdm
-import pyewf
+import subprocess
+
+def image_mount(input):
+    temporary_dir = input("Give a temporary mounting directory: ")
+    #Checken of de directory bestaat. Indien dit niet het geval is wordt er gevraagd of deze aangemaakt moet worden.
+    while os.path.isdir(temporary_dir) == False:
+        create_dir = input("Directory does not exist. Do you want it to be created? Yes/No ")
+        if create_dir == "Yes" or create_dir == "Y" or create_dir == "y":
+            subprocess.call(["sudo", "mkdir", temporary_dir])
+            print("Directory created")
+        else:
+            print("Please select another mounting directory.")
+        temporary_dir = input("Give a temporary mounting directory: ")
+    #De image wordt read only gemount met een bash commando
+    subprocess.call(["sudo", "mount", "-o", "ro", input, temporary_dir])
+    return temporary_dir
+
 
 def file_list(input):
     test = User_Interface.Main_program()
+    #Image mount wordt aangeroepen om de meegegeven image te mounten
+    sec_input = image_mount(input)
     #Dictionary om alle files met hashes erbij op te slaan
     file_dict = {}
     # Loop die een variabele met het path naar een file update zodat iedere file in de directory wordt afgelopen.
     # Vervolgens worden de subdir en de file gejoind in de variabele current_dir
     # Hierna wordt een betreffende directory meegegeven aan een splitext commando die de extensie van de file afhaald
-    for subdir, dirs, files in tqdm(os.walk(input)):
+    for subdir, dirs, files in tqdm(os.walk(sec_input)):
         for file in files:
             current_dir = (os.path.join(subdir, file))
             hash_waarde = User_Interface.Main_program.bereken_hash(test,current_dir)
