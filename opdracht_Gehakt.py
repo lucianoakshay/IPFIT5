5ced138fad758a6980bc24e530a82f4dddc48f09
devsda12 <jmbalster@hotmail.com>
2018-03-14 15:23:13 +0100

    Made a main in gehakt

--- opdracht_Gehakt.py
+++ opdracht_Gehakt.py
@@ -5,11 +5,15 @@ import subprocess
 
 class gehakt:
 
+    def main_def(self, input):
+        # Image mount wordt aangeroepen om de meegegeven image te mounten
+        sec_input = self.image_mount(input)
+
     #Function to mount e01 or dd images
     #E01 is not implemented yet
     def image_mount(self, given_dir):
         temporary_dir = input("Give a temporary mounting directory: ")
-        User_Interface.Main_program.Logging().info("")
+        User_Interface.Main_program.Logging().info("User defined mounting directory: " + temporary_dir)
         # Checken of de directory bestaat. Indien dit niet het geval is wordt er gevraagd of deze aangemaakt moet worden.
         while os.path.isdir(temporary_dir) == False:
             create_dir = input("Directory does not exist. Do you want it to be created? Yes/No ")
@@ -18,15 +22,13 @@ class gehakt:
                 print("Directory created")
             else:
                 print("Please select another mounting directory.")
-            temporary_dir = input("Give a temporary mounting directory: ")
+                temporary_dir = input("Give a temporary mounting directory: ")
         # De image wordt read only gemount met een bash commando
         subprocess.call(["sudo", "mount", "-o", "ro", given_dir, temporary_dir])
         return temporary_dir
 
-    def file_list(self, input):
+    def file_list(self, sec_input):
         #test = User_Interface.Main_program()
-        # Image mount wordt aangeroepen om de meegegeven image te mounten
-        sec_input = self.image_mount(input)
         # Dictionary om alle files met hashes erbij op te slaan
         file_dict = {}
         # Loop die een variabele met het path naar een file update zodat iedere file in de directory wordt afgelopen.
