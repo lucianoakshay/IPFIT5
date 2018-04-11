bebdb55979830e22cde23451a30038e938d7fad3
jasper <jmbalster@hotmail.com>
2018-04-11 12:19:50 +0200

    DD type checker added

--- User_Interface.py
+++ User_Interface.py
@@ -176,11 +176,11 @@ Menu
     def Gehakt_script(self):
         self.Logging().info("Starting Gehakt_Script")
         gehakt_image = input("Input the location of the image (.dd / .e01(not yet implemented)): ")
-        while os.path.isfile(gehakt_image) == False:
-            print(gehakt_image + " is not an file. Please try again")
+        filename, file_extension = os.path.splitext(gehakt_image)
+        while file_extension != ".dd":
+            print(gehakt_image + " is of type: " + file_extension + ". Please try again")
             gehakt_image = input("Input the location of the image (.dd / .e01(not yet implemented)): ")
         opdracht_Gehakt.gehakt.main_def(opdracht_Gehakt.gehakt(), gehakt_image)
-        print("This is the Gehakt_script that's now running")
 
     # will be used in the future to ask for input of the e01 file
     def input_e01_file(self):
