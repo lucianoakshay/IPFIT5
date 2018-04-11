4c629ed9bc3239a3f197f47832b86f497d221dfd
Luciano Chanchal <luciano_a7@hotmail.com>
2018-04-11 14:47:48 +0200

    deleted table.py and working with userinterface

--- opdracht_foto.py
+++ opdracht_foto.py
@@ -2,38 +2,39 @@ from __future__ import print_function
 from PIL import Image
 from PIL.ExifTags import TAGS, GPSTAGS
 from gmplot import gmplot
-from tqdm import tqdm
 import sys, os
 import tkinter as tk
 from tkinter import filedialog
 import texttable as tt
-import table
 import User_Interface as ui
-import csv
-import main as fotomain
+import foto_main as fotomain
 import pyewf
 import pytsk3
+from io import BytesIO
 
 
 def get_exif_data(image):
     """
     Returns a dictionary from the exif data of a PIL Image item. Also converts the GPS Tags.
     """
-    exif_data = {}
-    info = Image.open(BytesIO(image))
-    if info:
-        for tag, value in info.items():
-            decoded = TAGS.get(tag, tag)
-            if decoded == "GPSInfo":
-                gps_data = {}
-                for t in value:
-                    sub_decoded = GPSTAGS.get(t, t)
-                    gps_data[sub_decoded] = value[t]
+    try:
+        exif_data = {}
+        image = Image.open(BytesIO(image))
+        info = image._getexif()
+        if info:
+            for tag, value in info.items():
+                decoded = TAGS.get(tag, tag)
+                if decoded == "GPSInfo":
+                    gps_data = {}
+                    for t in value:
+                        sub_decoded = GPSTAGS.get(t, t)
+                        gps_data[sub_decoded] = value[t]
 
-                exif_data[decoded] = gps_data
-            else:
-                exif_data[decoded] = value
-
+                    exif_data[decoded] = gps_data
+                else:
+                    exif_data[decoded] = value
+    except Exception as ex:
+        pass
     return exif_data
 
 
@@ -136,7 +137,7 @@ def get_lat_lon_from_imagefile(image):
     """
     Returns the latitude and longitude, if available, for a given image file.
     """
-    image = Image.open(BytesIO(image))
+    #image = Image.open(BytesIO(image))
     exif_data = get_exif_data(image)
 
     return get_lat_lon(exif_data)
@@ -145,7 +146,7 @@ def get_lat_from_imagefile(image):
     """
     Returns the LAT if available, for a given image file.
     """
-    image = Image.open(BytesIO(image))
+    #image = Image.open(BytesIO(image))
     exif_data = get_exif_data(image)
 
     return get_lat(exif_data)
@@ -154,7 +155,7 @@ def get_lon_from_imagefile(image):
     """
     Returns the LONGitude, if available, for a given image file.
     """
-    image = Image.open(BytesIO(image))
+    #image = Image.open(BytesIO(image))
     exif_data = get_exif_data(image)
 
     return get_lon(exif_data)
@@ -167,122 +168,43 @@ def mapplotter(filename):
 
     return None
 
-def moremapplotter(image, aantal):
+def moremapplotter(image):
         gmap = gmplot.GoogleMapPlotter(52.370216, 4.895168, 8)
-#        file_lat, file_lon = get_lat_lon_from_imagefile(filename)
-#        gmap.marker(lat, lon, 'cornflowerblue')
-#        gmap.draw("my_map_2.html")
 
-#        if os.path.isfile(filename):
-#            for file in os.listdir("./fotos"):
-#                print(os.path.join(file))
-#        print(get_lat_lon_from_imagefile(filename))
+        myLat = []
+        myLon = []
 
-#        myList = filesinmap(foldername)
-        myextraList = []
+        for x in image:
 
-        for x in aantal:
-            myextraList.append(get_lat_lon_from_imagefile(image))
-#            myextraList.append(file_lat, file_lon = get_lat_lon_from_imagefile(x))
-        for x in myextraList:
-#            gmap.marker(x, 'cornflowerblue')
-#            print(foldername + '/' + x)
-#        myextraList.split(",")
-#        print(myextraList)
-            lats, lons = zip(*[x])
-#            print(x)
-            gmap.scatter(lats, lons, 'cornflowerblue', edge_width=100)
-#            gmap.marker(51.904444444444444, 4.351388888888889, 'cornflowerblue')
-        gmap.draw("my_map_3.html")
+            lon = (get_lon_from_imagefile(x))
+            lat = (get_lat_from_imagefile(x))
+
+            if type(lon) == int or type(lon) == float:
+                myLon.append(lon)
+
+            if type(lat) == int or type(lat) == float:
+                myLat.append(lat)
+
+        gmap.scatter(myLat, myLon, 'cornflowerblue', edge_width=100)
+
+        gmap.draw("Export_Map.html")
 
         return None
 
-
-def exifinformatie(image):
-    # https://www.awaresystems.be/imaging/tiff/tifftags/privateifd/exif.html
-    # Datum + tijd = 36867
-    exif_data = {}
-    image = Image.open(BytesIO(image))
-    info = image._getexif()
-    model = "Model"
-    make = "Make"
-    datum = "DateTimeOriginal"
-    datum1 ="DateTimeDigitized"
-    datum2 = "DateTime"
-
-    lijst = ['Make', 'Model']
-    namelist = ['Merk', 'Model']
-    if info:
-        for x in lijst:
-            for tag, value in info.items():
-                decoded = TAGS.get(tag, tag)
-                if decoded == x:
-                    listindex = lijst.index(x)
-                    print(namelist[listindex])
-                    print(value)
-
-            # if decoded == model:
-            #     print("Model")
-            #     print(value)
-            # if decoded == make:
-            #     print("Merk")
-            #     print(value)
-            #     gps_data = {}
-            #     for t in value:
-            #         sub_decoded = GPSTAGS.get(t, t)
-            #         gps_data[sub_decoded] = value[t]
-            #
-            #     exif_data[decoded] = gps_data
-            # else:
-            #     exif_data[decoded] = value
-
-    return None
-#    return Image.open(filename)._getexif()[271][272]
-
-
-def voorbeeld(image):
-    """
-    Returns a dictionary from the exif data of a PIL Image item. Also converts the GPS Tags.
-    """
-    exif_data = {}
-    info = image._getexif()
-    if info:
-        for tag, value in info.items():
-            decoded = TAGS.get(tag, tag)
-            if decoded == "GPSInfo":
-                gps_data = {}
-                for t in value:
-                    sub_decoded = GPSTAGS.get(t, t)
-                    gps_data[sub_decoded] = value[t]
-
-                exif_data[decoded] = gps_data
-            else:
-                exif_data[decoded] = value
-
-    return exif_data
-
-
 def allebestanden(filepath):
+    fotomain.EWFImgInfo.allfiles(filepath)
 
-    #filepath = "./imageusbstick.E01"
-    fotomain.EWFImgInfo.hashes(filepath)
-
-def allecamerabestanden():
-
-    filepath = "./imageusbstick.E01"
+def allecamerabestanden(filepath):
     fotomain.EWFImgInfo.onlyphotofiles(filepath)
 
-def showcameras():
-
-    filepath = "./imageusbstick.E01"
+def showcameras(filepath):
     fotomain.EWFImgInfo.showcameras(filepath)
 
-def fotosbijcamera():
-
-    filepath = "./imageusbstick.E01"
+def fotosbijcamera(filepath):
     fotomain.EWFImgInfo.fotosbijcamera(filepath)
 
-def exif_locatie():
-
-    filepath = "./imageusbstick.E01"
+def exif_locatie(filepath):
     fotomain.EWFImgInfo.exif_locatie(filepath)
+
+def exifinformatie(filepath):
+    fotomain.EWFImgInfo.exifinformatie(filepath)
