98d52bd9752fc9f15ebd0386240528a2278df4e6
koen <koenvandersijs@hotmail.com>
2018-04-12 22:39:06 +0200

    Added apt-get install for python3-tk module

--- InstallDependencies.py
+++ InstallDependencies.py
@@ -1,8 +1,11 @@
 import pip
 import os
 import sys
+from subprocess import STDOUT, check_call
 
 def installer (requirements):
+    check_call(['apt-get', 'install', '-y', 'python3-tk'],
+     stdout=open(os.devnull,'wb'), stderr=STDOUT)
     if os.path.isfile(requirements):
         pip.main(["install", "-r", str(requirements)])
     else:
